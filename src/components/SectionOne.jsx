5e2028e34147a8e401c3e38399fe4ceb6d4d836a
mzs21 <snigdho2111@gmail.com>
2023-03-10 20:50:34 +0600

    add: text animation

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import Splitting from "splitting";
 
 const SectionOne = () => {
   return (
-    <section className="flex justify-center">
-      <div className="section-one-text glass-effect ">
-        <h3 className="section-one-headline">Be Curious About What Matters</h3>
-        <p className="max-w-2xl font-light">
-          Unlock the world of knowledge, investigate the unknown, and be engaged
-          in the innovative journey.
-        </p>
-      </div>
+    // <section className="flex justify-center">
+    //   <div className="section-one-text glass-effect ">
+    //     <h3 className="section-one-headline">Be Curious About What Matters</h3>
+    //     <p className="max-w-2xl font-light">
+    //       Unlock the world of knowledge, investigate the unknown, and be engaged
+    //       in the innovative journey.
+    //     </p>
+    //   </div>
+    // </section>
+    <section class="title my-40 relative text-center" style={{ transform: "" }}>
+      <Splitting />
+      <span class="text write" data-splitting="lines">
+        Be Curious About What Matters
+        <br />
+        Unlock the world of knowledge, investigate the unknown, and be engaged
+        in the innovative journey.
+      </span>
+      <span aria-hidden="true" class="text laser" data-splitting="lines">
+        Be Curious About What Matters
+        <br />
+        Unlock the world of knowledge, investigate the unknown, and be engaged
+        in the innovative journey.
+      </span>
     </section>
   );
 };
