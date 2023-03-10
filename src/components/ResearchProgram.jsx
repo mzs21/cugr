4c3f1487d3f5409124603cff310a4a39947f3555
mzs21 <snigdho2111@gmail.com>
2023-03-10 18:24:44 +0600

    remove: unnecessary text

--- src/components/ResearchProgram.jsx
+++ src/components/ResearchProgram.jsx
@@ -1,42 +1,42 @@
 import React from "react";
 const ResearchProgram = () => {
-    // const info = [
-    //   {
-    //     id: 1,
-    //     heading: `Introductory Class: "ResIntro"`,
-    //     PointOne:
-    //       "Kick off your research journey with ResIntro, where you'll get an introduction to research and an understanding of our course goals.",
-    //     PointTwo:
-    //       "Learn about Original Work and Review Work details, and what's expected of you during the program.",
-    //   },
+  // const info = [
+  //   {
+  //     id: 1,
+  //     heading: `Introductory Class: "ResIntro"`,
+  //     PointOne:
+  //       "Kick off your research journey with ResIntro, where you'll get an introduction to research and an understanding of our course goals.",
+  //     PointTwo:
+  //       "Learn about Original Work and Review Work details, and what's expected of you during the program.",
+  //   },
 
-    //   {
-    //     id: 2,
-    //     heading: `Pre Research Skill Workshops: "SkillLab"`,
-    //     PointOne:
-    //       "Get equipped with the essential skills you need to excel in research with our SkillLab.",
-    //     PointTwo:
-    //       "Master Google docs, forms, sheets & their formatting, zotero, data analysis, basic graph creation with sheets, citation style,paraphrasing & plagiarism, paper finding, reading, article search & download, and ethics of research.",
-    //   },
+  //   {
+  //     id: 2,
+  //     heading: `Pre Research Skill Workshops: "SkillLab"`,
+  //     PointOne:
+  //       "Get equipped with the essential skills you need to excel in research with our SkillLab.",
+  //     PointTwo:
+  //       "Master Google docs, forms, sheets & their formatting, zotero, data analysis, basic graph creation with sheets, citation style,paraphrasing & plagiarism, paper finding, reading, article search & download, and ethics of research.",
+  //   },
 
-    //   {
-    //     id: 3,
-    //     heading: `Research Work: "ResearchHub"`,
-    //     PointOne:
-    //       "  Join the ResearchHub, where our experienced mentors will guide you to select a paper topic and assign tasks.",
-    //     PointTwo:
-    //       "Ensure you meet all deadpnes with the help of your research guide and attend weekly 2 research meetings with your mentor, a well as 3 weekly 1:1 checkups.",
-    //   },
+  //   {
+  //     id: 3,
+  //     heading: `Research Work: "ResearchHub"`,
+  //     PointOne:
+  //       "  Join the ResearchHub, where our experienced mentors will guide you to select a paper topic and assign tasks.",
+  //     PointTwo:
+  //       "Ensure you meet all deadpnes with the help of your research guide and attend weekly 2 research meetings with your mentor, a well as 3 weekly 1:1 checkups.",
+  //   },
 
-    //   {
-    //     id: 4,
-    //     heading: `After Research Work: "PaperCraft"`,
-    //     PointOne:
-    //       "Take your research skills to the next level with PaperCraft.",
-    //     PointTwo:
-    //       "Craft an impressive research paper with guidance on abstract, introduction, methodology, results, conclusion, reference,figures, title, and keyword.",
-    //   },
-    // ];
+  //   {
+  //     id: 4,
+  //     heading: `After Research Work: "PaperCraft"`,
+  //     PointOne:
+  //       "Take your research skills to the next level with PaperCraft.",
+  //     PointTwo:
+  //       "Craft an impressive research paper with guidance on abstract, introduction, methodology, results, conclusion, reference,figures, title, and keyword.",
+  //   },
+  // ];
   return (
     <>
       <div className="introTwo">
@@ -119,7 +119,9 @@ const ResearchProgram = () => {
       </div>
 
       <div className="introThree">
-        <h2><b>How to Apply for the Centre for Undergraduate Research</b></h2>
+        <h2>
+          <b>How to Apply for the Centre for Undergraduate Research</b>
+        </h2>
         <p>
           Ready to embark on an exciting journey of discovery and personal
           growth with the Centre for Undergraduate Research? Applying is simple
@@ -177,62 +179,6 @@ const ResearchProgram = () => {
           and become part of a community of learners dedicated to making a
           difference in the world.
         </p>
-        <p>
-          Write a "Program Details" website section (with bullet points
-          highlighting differentiators) for the website of{" "}
-          <b>Centre for Undergraduate Research</b> which provides affordable
-          research opportunities for Bangladeshi undergraduates, has nice
-          faculty and academic team, has many research collaborations with other
-          organizations, and is trying to make research popular in Bangladesh.
-          It has several parts: Introductory class, Pre Research skill
-          workshops, Research Work, After Research Work.{" "}
-        </p>
-        <p>
-          {" "}
-          <b> Introductory class:</b>
-        </p>
-        <ul className="list-alpha list-inside">
-          <li>Intro to research + Course Goal </li>
-          <li>Original work and review work details</li>
-        </ul>
-        <p>
-          {" "}
-          <b>Pre Research skill workshops:</b>
-        </p>
-
-        <ul className="list-alpha list-inside">
-          <li>Google docs, forms, sheets & their formatting</li>
-          <li>Zotero</li>
-          <li>Data analysis, Basic graph creation with sheets</li>
-          <li>Citation style</li>
-          <li>Paraphrasing & plagiarism </li>
-          <li>
-            Paper finding, Reading, Article search & download, ethics of
-            research
-          </li>
-        </ul>
-
-        <p>
-          {" "}
-          <b>Research Work:</b>{" "}
-        </p>
-        <ul className="list-alpha list-inside">
-          <li>Mentor will fix paper topic & task division</li>
-          <li>Research guide will ensure participants meeting deadlines</li>
-          <li>Weekly 2 research meetings with research mentor</li>
-          <li>Weekly 3 1:1 checkups by research guide</li>
-        </ul>
-
-        <p>
-          {" "}
-          <b>After research part:</b>{" "}
-        </p>
-        <ul className="list-alpha list-inside">
-          <li>Paper writing</li>
-          <li>Abstract, introduction</li>
-          <li>Methodology, Result, conclusion</li>
-          <li>Reference, figures, title, keyword</li>
-        </ul>
       </div>
     </>
   );
